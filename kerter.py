6ee5a01acf4ffdb0dd07df8b7e59169b73411742
AfRiyy <madaraszdavidd@gmail.com>
2020-10-07 10:36:22 +0200

    javítasok

--- kerter.py
+++ kerter.py
@@ -1,15 +1,13 @@
-number1 = 0
-number2 = 0
-
-number1 = float(input (" Kerület <cm>: "))
-number2 = float(input (" Terület <cm>: "))
-
-	
+#Ide ne írjatok egyedi bekérést, azt a def-be rakjátok pl def METÓDUSNEVE(szam1,szam2 stb..): és utána a DEF-ben kérjetek be számot az usertől
+#Írta:
+def haromszogKerulet(szam):
 	return szam
-result = number1*number2
+def haromszogTerulet(szam):
 	return szam
 #Írta:
-	return szam
+def korTerulet(szam):
+	result = number1*number2
+	return result
 def korTerulet(szam):
 	return szam
 	
@@ -18,8 +16,9 @@ def teglalapKerulet(szam):
 	return szam
 def teglalapTerulet(szam):
 	return szam
+	
+	
 #Írta:Patrícia
-
 	a=float(input("Adja meg a téglalap a oldalát <cm>:"))
 	b=float(input("Adja meg a téglalap b oldalát <cm>:"))
 def teglalapKerulet(szam):
@@ -28,8 +27,8 @@ def teglalapKerulet(szam):
 def teglalapTerulet(szam):
 	szam2= a*b
 	return szam2
-print (szam1, "cm2 a téglalap kerülete ", szam2, "a téglalap területe")
-
+	
+	
 #Írta:
 def nyolcszogKerulet(szam):
 	return szam
@@ -46,18 +45,20 @@ print("4: Nyolcszög terület-kerület")
 szamValasztas=int(input("Írd be a megfelelő számot, melyik induljon el:"))
 #Háromszög
 if szamValasztas == 1:
-	haromszogKerulet(szam)
-	haromszogTerulet(szam)
+	print("A háromszög kerülete",haromszogKerulet(szam))
+	print("A háromszög területe",haromszogTerulet(szam))
 #Kör
 elif szamValasztas == 2:
-	korKerulet(szam)
-	korTerulet(szam)
+	print("A kör kerülete",korKerulet(szam))
+	print("A kör területe",korTerulet(szam))
 #Téglalap
 elif szamValasztas == 3:
-	teglalapKerulet(szam)
-	teglalapTerulet(szam)
+	print("A téglalap kerülete",teglalapKerulet(a,b))
+	print("A téglalap területe",teglalapTerulet(a,b))
 #Nyolcszög
 elif szamValasztas == 4:
-	nyolcszogKerulet(szam)
-	nyolcszogTerulet(szam)
+	print("A nyolcszög kerülete",nyolcszogKerulet(szam))
+	print("A nyolcszög területe",nyolcszogTerulet(szam))
+else:
+	print("Nem jó számot írtál be!")
 	
