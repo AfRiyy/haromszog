f0a1fe082fb91e753e30c34dd12347e7031f06ea
AfRiyy <madaraszdavidd@gmail.com>
2020-10-07 10:11:08 +0200

    new version

--- kerter.py
+++ kerter.py
@@ -1,17 +1,41 @@
-#input
-szam=input("�rj be egy sz�mot:")
-#�rta:
+
+#Írta:
 def haromszogKerulet(szam):
+	
+	return szam
 def haromszogTerulet(szam):
-#�rta:
+	return szam
+#Írta:
 def korKerulet(szam):
+	return szam
 def korTerulet(szam):
-#�rta:
+	return szam
+#Írta:
 def teglalapKerulet(szam):
+	return szam
 def teglalapTerulet(szam):
-#�rta:
+	return szam
+#Írta:
 def nyolcszogKerulet(szam):
+	return szam
 def nyolcszogTerulet(szam):
+	return szam
 
 
-#F� program: D�vid
+#Fő program: Dávid
+print("Válassz:")
+print("1: Háromszög terület-kerület")
+print("2: Kör terület-kerület")
+print("3: Téglalap terület-kerület")
+print("4: Nyolcszög terület-kerület")
+valasztasSzam=int(input("Írd be a megfelelő számot, melyik induljon el:"))
+def valasztas(valasztasSzam):
+	switcher={
+	1:
+		print("Teszt1!")
+	2:
+	3:
+	4:
+	}
+	return switcher.get(valasztasSzam,"nem  jó szöveget írtál be!")
+	
