59eee2dce2b1172fedca1e08a29c3917925a9463
AfRiyy <madaraszdavidd@gmail.com>
2020-10-07 09:55:26 +0200

    mehet a munka chop-chop!

---
+++ kerter.py
@@ -0,0 +1,17 @@
+#input
+szam=input("Írj be egy számot:")
+#Írta:
+def haromszogKerulet(szam):
+def haromszogTerulet(szam):
+#Írta:
+def korKerulet(szam):
+def korTerulet(szam):
+#Írta:
+def teglalapKerulet(szam):
+def teglalapTerulet(szam):
+#Írta:
+def nyolcszogKerulet(szam):
+def nyolcszogTerulet(szam):
+
+
+#Fõ program: Dávid
