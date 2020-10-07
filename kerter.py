29ec0853c4bcaab9d4e2b2252cbe8d0c5123f7c4
Lehoczki Patrícia <lehoczkipatricia99@gmail.com>
2020-10-07 10:09:21 +0200

    téglalap hozzáadva

--- kerter.py
+++ kerter.py
@@ -6,9 +6,18 @@ def haromszogTerulet(szam):
 #Írta:
 def korKerulet(szam):
 def korTerulet(szam):
-#Írta:
+
+#Írta: Patorícia
+	a=float(input(Adja meg a téglalap a oldalát <cm>: ))
+	b=float(input(Adja meg a téglalap b oldalát <cm>: ))
 def teglalapKerulet(szam):
+	szam1=a*2 + b*2
+	return szam1
 def teglalapTerulet(szam):
+	szam2= a*b
+	return szam2
+print (szam1, "cm2 a téglalap kerülete és", szam2, "a téglalap területe"
+
 #Írta:
 def nyolcszogKerulet(szam):
 def nyolcszogTerulet(szam):
